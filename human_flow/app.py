3cfb7fa10b923e974c4ce631d645e460c206d6e3
Jerry Pussinen <jerry.pussinen@gmail.com>
2019-11-17 04:08:09 +0200

    Add predictions and weather sampling

--- human_flow/app.py
+++ human_flow/app.py
@@ -15,14 +15,22 @@ bike_drives_df = pd.read_csv(
     DRIVES_DATA_CSV, parse_dates=["departure_time", "return_time"]
 )
 
+PREDICTIONS_CSV = str(Path(__file__).parents[1] / "data" / "predictions.csv")
+predicted_drives_df = pd.read_csv(
+    DRIVES_DATA_CSV, parse_dates=["departure_time", "return_time"]
+)
+
 
 WEATHER_DIR = str(
-    Path(__file__).parents[1] / "data" / "clean" / "weather" / "2019-07-09.csv")
+    Path(__file__).parents[1] / "data" / "clean" / "weather" / "2019-07-09.csv"
+)
 weather_df = pd.read_csv(WEATHER_DIR, parse_dates=["time"])
+weather_df = weather_df.iloc[::6, :]
 
 
 app = Flask(__name__)
 CORS(app)
 app.bike_drives_df = bike_drives_df
 app.weather_df = weather_df
+app.predicted_drives_df = predicted_drives_df
 from human_flow import routes
